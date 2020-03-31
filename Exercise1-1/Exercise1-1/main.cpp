29c669802e6c99d65d9d754a7e2584b4f781fbea
Greg Tosato <gregtosato@gmail.com>
2020-03-31 14:55:04 +1100

    Completed version of Exercise1-1.

---
+++ Exercise1-1/Exercise1-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Exercise1-1
+//
+//  Created by Greg Tosato on 31/3/20.
+//  Copyright © 2020 Dingo Byte Solutions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "This is exercise 1-1.\n";
+    return 0;
+}
